c126dd323c3cdeae499ce4f21b42cb5d3106cfed
Anurag Anand <anuraganand789@hotmail.com>
2020-09-03 20:53:37 +0530

    code to learn data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+  clickCount: number = 0;
+  textBoxValue: string = "";
+  onClick(): void {
+      ++(this.clickCount);
+  }
+
+  onKey(domEvent: KeyboardEvent): void {
+      this.textBoxValue += (domEvent.target as HTMLInputElement).value + '|';
+  }
+
+  onKeyByValue(value: string): void {
+      this.textBoxValue += value + '|';
+  }
+
+  resetTextValue(box: any): void{
+      this.textBoxValue = '';
+      box.value = '';
+  }
+
+  keyEnterValue: string;
+
+  onKeyEnter(value: string): void{
+    this.keyEnterValue = value;
+  }
+
+  listOfStrings: Array<string>;
+  
+  addString(value: string): void{
+    if(!this.listOfStrings) this.listOfStrings = [];
+    this.listOfStrings.push(value);
+  }
+
 }
